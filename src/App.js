e40898cccee5cdea82c16cf8ff45e5374e0d5801
windaAgusthia <winda.agusthia@codify.id>
2022-01-08 17:12:58 +0700

    change landing page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { Flex } from '@chakra-ui/react';
+import { Provider } from 'react-redux';
+import Home from './component/Home';
+import store from './store/indexStore';
 
-function App() {
+export const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Provider store={store}>
+      <Flex w='100%'>
+        <Home />
+      </Flex>
+    </Provider>
   );
 }
 
